7114c5036e3ad861a57f135449af2b79c0c50275
Paul Spain <pvspain@gmail.com>
2014-12-15 17:55:59 +1100

    finalised. working

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,34 +2,47 @@ package clock
 
 import "fmt"
 
-func saneHour(hour int) int {
-	if hour >= 0 {
-		return hour % 24
+func normaliseHour(h int) int {
+	r := h % 24
+	if h >= 0 {
+		return r
+	}
+	if r != 0 {
+		return 24 + r
 	}
 	return 0
 }
 
-func saneMinute(minute int) int {
-	if minute >= 0 {
-		return minute % 60
+func normaliseMinute(m int) (hours int, minutes int) {
+	d, r := m/60, m%60
+	if m >= 0 {
+		return d, r
 	}
-	return 0
+	if r != 0 {
+		return d - 1, 60 + r
+	}
+	return d, 0
 }
 
 type Clock struct {
 	hour, minute int
 }
 
-func New(hour, minute int) Clock {
-	return Clock{saneHour(hour), saneMinute(minute)}
+func New(h, m int) *Clock {
+	c := new(Clock)
+	carry := 0
+	carry, c.minute = normaliseMinute(m)
+	c.hour = normaliseHour(h + carry)
+	return c
 }
 
-func (clock *Clock) String() string {
-	return fmt.Sprintf("%.2d:%.2d", clock.hour, clock.minute)
+func (c *Clock) String() string {
+	return fmt.Sprintf("%.2d:%.2d", c.hour, c.minute)
 }
 
-func (clock *Clock) Add(minutes int) {
-	sumMinutes := clock.minute + minutes
-	clock.minute = saneMinute(sumMinutes)
-	clock.hour = saneHour(clock.hour + sumMinutes/60)
+func (c *Clock) Add(minutes int) *Clock {
+	hours := 0
+	hours, c.minute = normaliseMinute(c.minute + minutes)
+	c.hour = normaliseHour(c.hour + hours)
+	return c
 }
